7296bc8ecf3c5762645459608188d76e7492e76e
jrshort89 <jrshort89@gmail.com>
2020-11-19 12:42:44 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const divElm = document.querySelector('div');
+
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            name: event.target.userName.value,
+            email: event.target.email.value
+        })
+    }).then(response => {
+        return response.json();
+    }).then(data => {
+        const elm = document.createElement('p')
+        elm.innerText = data.id;
+        divElm.append(elm);
+    }).catch(err => {
+        const errorElm = document.createElement('p');
+        errorElm.innerText = err;
+        divElm.append(errorElm);
+        console.log(err);
+    });
+})
